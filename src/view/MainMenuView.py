7b74a28554e85dc1309518f4dae4e078b2c1be9b
Scott Moore <smoore@cyberitas.com>
2022-11-05 01:38:53 -0700

    Added keyboard menu selection logic

--- src/view/MainMenuView.py
+++ src/view/MainMenuView.py
@@ -21,6 +21,7 @@ class MainMenuView(AbstractView):
         self.emitter = None
         self.soundPlayer = None
         self.emitter_timeout = 0
+        self.selected = 0
 
     def create_wand_emitter(self):
         center = self.game_width / 2 + self.scaled(WAND_OFFSET_X), self.game_height / 2 + self.scaled(WAND_OFFSET_Y)
@@ -52,10 +53,28 @@ class MainMenuView(AbstractView):
         super().on_resize(width, height)
         self.emitter = self.create_wand_emitter()
 
-    def draw_menu_choice(self, text, count):
+    def draw_menu_choice(self, text, fullSize, count):
         y_unscaled = MENU_TOP_POS - MENU_TOP_PAD - (count * MENU_LINE_HEIGHT)
+        # Set the color based on if it's selected or not
+        active = self.selected == count
+        if active:
+            color = arcade.color.BLACK
+            arcade.draw_circle_filled(self.scaled(MENU_CENTER_POS - fullSize), self.scaled(y_unscaled)+self.scaled(12),
+                                      self.scaled(10), color)
+            arcade.draw_line(self.scaled(MENU_CENTER_POS - fullSize - 40), self.scaled(y_unscaled) + self.scaled(12),
+                             self.scaled(MENU_CENTER_POS - fullSize), self.scaled(y_unscaled) + self.scaled(12),
+                             color, self.scaled(2))
+            arcade.draw_circle_filled(self.scaled(MENU_CENTER_POS + fullSize),
+                                      self.scaled(y_unscaled) + self.scaled(12),
+                                      self.scaled(10), color)
+            arcade.draw_line(self.scaled(MENU_CENTER_POS + fullSize), self.scaled(y_unscaled) + self.scaled(12),
+                             self.scaled(MENU_CENTER_POS + fullSize + 40), self.scaled(y_unscaled) + self.scaled(12),
+                             color, self.scaled(2))
+        else:
+            color = arcade.color.GRAY
+
         arcade.draw_text(text, self.scaled(MENU_CENTER_POS), self.scaled(y_unscaled),
-                         arcade.color.CHARCOAL,
+                         color,
                          font_size=self.scaled(MENU_CHOICE_FONT_SIZE), anchor_x="center",
                          font_name="Eagle Lake"
                          )
@@ -79,14 +98,37 @@ class MainMenuView(AbstractView):
                          font_name="Eagle Lake"
                          )
 
-        self.draw_menu_choice("New Game", 0)
-        self.draw_menu_choice("Load Game", 1)
-        self.draw_menu_choice("Options", 2)
-        self.draw_menu_choice("Quit", 3)
+        self.draw_menu_choice("New Game", 150, 0)
+        self.draw_menu_choice("Load Game", 160, 1)
+        self.draw_menu_choice("Options", 120, 2)
+        self.draw_menu_choice("Quit", 80, 3)
 
         # Draw the emitter related stuff
         if self.emitter:
             self.emitter.draw()
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
+        # If the user presses the mouse button, go to the menu choice they clicked.
+        # TODO: Figure out if the user clicked on the "New Game" button
         self.window.show_view(CinematicIntroView())
+
+    def on_key_press(self, key, _modifiers):
+        if key == arcade.key.UP:
+            self.selected -= 1
+        elif key == arcade.key.DOWN:
+            self.selected += 1
+
+        if self.selected < 0:
+            self.selected = 3
+        elif self.selected > 3:
+            self.selected = 0
+
+        if key == arcade.key.ENTER:
+            if self.selected == 0:
+                self.window.show_view(CinematicIntroView())
+            elif self.selected == 1:
+                print("Load Game")
+            elif self.selected == 2:
+                print("Options")
+            elif self.selected == 3:
+                pyglet.app.exit()
